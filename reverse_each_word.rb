b55055fcacd8cd2b159ec885cb09d21e7e30803b
Michael Takeuchi <miketaki@sbcglobal.net>
2020-05-26 21:33:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   arr = string.split(" ")
-  reversed = arr.map { |word| word.split.reverse }
+  reversed = arr.map { |word| word.split("").reverse }
   puts reversed.join(" ")
 end
